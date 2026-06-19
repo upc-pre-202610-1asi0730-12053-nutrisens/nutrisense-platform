9eb83d9960045d02b12c339785c8f3a3accd6253
Angel Villarreal Bazan <angelvillarrealbazan@gmail.com>
2026-06-19 17:37:13 -0500

    feat(iam): add DeleteUserCommand

---
+++ Nutrisense.Nutrisense.Platform/IAM/Domain/Model/Commands/DeleteUserCommand.cs
@@ -0,0 +1,5 @@
+using Nutrisense.Nutrisense.Platform.IAM.Domain.Model.ValueObjects;
+
+namespace Nutrisense.Nutrisense.Platform.IAM.Domain.Model.Commands;
+
+public record DeleteUserCommand(UserId UserId);
