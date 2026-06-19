893f8a4921f6c3ffab31598d042026ba06a53265
Angel Villarreal Bazan <angelvillarrealbazan@gmail.com>
2026-06-19 17:37:44 -0500

    feat(iam): add GetUserByIdQuery

---
+++ Nutrisense.Nutrisense.Platform/IAM/Domain/Model/Queries/GetUserByIdQuery.cs
@@ -0,0 +1,5 @@
+using Nutrisense.Nutrisense.Platform.IAM.Domain.Model.ValueObjects;
+
+namespace Nutrisense.Nutrisense.Platform.IAM.Domain.Model.Queries;
+
+public record GetUserByIdQuery(UserId Id);
