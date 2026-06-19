60f9d6094f725f7ddf751af2e579128143364a91
Angel Villarreal Bazan <angelvillarrealbazan@gmail.com>
2026-06-19 17:20:18 -0500

    feat(shared): add persistence base (dbcontext, unit of work, interceptors)

---
+++ Nutrisense.Nutrisense.Platform/Shared/Infrastructure/Persistence/EFC/Repositories/BaseRepository.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Nutrisense.Nutrisense.Platform.Shared.Domain.Repositories;
+using Nutrisense.Nutrisense.Platform.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace Nutrisense.Nutrisense.Platform.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+public class BaseRepository<TEntity>(AppDbContext context) : IBaseRepository<TEntity> where TEntity : class
+{
+    protected readonly AppDbContext Context = context;
+
+    public async Task AddAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        await Context.Set<TEntity>().AddAsync(entity, cancellationToken);
+    }
+
+    public async Task<TEntity?> FindByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        return await Context.Set<TEntity>().FindAsync([id], cancellationToken);
+    }
+
+    public void Update(TEntity entity)
+    {
+        Context.Set<TEntity>().Update(entity);
+    }
+
+    public void Remove(TEntity entity)
+    {
+        Context.Set<TEntity>().Remove(entity);
+    }
+
+    public async Task<IEnumerable<TEntity>> ListAsync(CancellationToken cancellationToken = default)
+    {
+        return await Context.Set<TEntity>().ToListAsync(cancellationToken);
+    }
+}
