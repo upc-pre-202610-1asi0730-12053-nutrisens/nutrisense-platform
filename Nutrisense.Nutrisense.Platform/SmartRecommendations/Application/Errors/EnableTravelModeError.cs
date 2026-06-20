8151803ce17a13ce75aae16e4e4c9ca7e583bda6
Angel Villarreal Bazan <angelvillarrealbazan@gmail.com>
2026-06-19 20:31:39 -0500

    feat(smart-recommendations): add application errors

---
+++ Nutrisense.Nutrisense.Platform/SmartRecommendations/Application/Errors/EnableTravelModeError.cs
@@ -0,0 +1,8 @@
+namespace Nutrisense.Nutrisense.Platform.SmartRecommendations.Application.Errors;
+
+public enum EnableTravelModeError
+{
+    CityNotFound,
+    PlanNotSufficient,
+    UnexpectedError
+}
---
+++ Nutrisense.Nutrisense.Platform/SmartRecommendations/Application/Errors/RegisterPantryItemsError.cs
@@ -0,0 +1,8 @@
+namespace Nutrisense.Nutrisense.Platform.SmartRecommendations.Application.Errors;
+
+public enum RegisterPantryItemsError
+{
+    IngredientNotFound,
+    PlanNotSufficient,
+    UnexpectedError
+}
