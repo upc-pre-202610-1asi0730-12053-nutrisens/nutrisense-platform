26cb4674a585e32ac62afc31fe6018d359207d14
olenkisha14 <olenkapriscilla.delaguila@gmail.com>
2026-06-19 20:57:13 -0500

    feat(analytics-reporting): add application errors

---
+++ Nutrisense.Nutrisense.Platform/AnalyticsReporting/Application/Errors/UpdateUsageStreakError.cs
@@ -0,0 +1,7 @@
+namespace Nutrisense.Nutrisense.Platform.AnalyticsReporting.Application.Errors;
+
+/// <summary>Failure reasons for updating a user's usage streak.</summary>
+public enum UpdateUsageStreakError
+{
+    UnexpectedError
+}
