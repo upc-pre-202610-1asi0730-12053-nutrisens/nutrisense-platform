3d55e2190f74cb6c21b00e91196c6c413775b6d3
Angel Villarreal Bazan <angelvillarrealbazan@gmail.com>
2026-06-19 20:33:44 -0500

    feat(smart-recommendations): register smart recommendations dependencies and dbsets

--- Nutrisense.Nutrisense.Platform/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ Nutrisense.Nutrisense.Platform/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using Nutrisense.Nutrisense.Platform.NutritionTracking.Domain.Model.Aggregates;
 using Nutrisense.Nutrisense.Platform.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using Nutrisense.Nutrisense.Platform.Shared.Infrastructure.Persistence.EFC.Interceptors;
+using Nutrisense.Nutrisense.Platform.SmartRecommendations.Domain.Model.Aggregates;
+using Nutrisense.Nutrisense.Platform.SmartRecommendations.Domain.Model.Entities;
 using Nutrisense.Nutrisense.Platform.Subscriptions.Domain.Model.Aggregates;
 
 namespace Nutrisense.Nutrisense.Platform.Shared.Infrastructure.Persistence.EFC.Configuration;
@@ -10,6 +12,10 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<SubscriptionPlan> SubscriptionPlans { get; init; }
     public DbSet<Food> Foods { get; init; }
+    public DbSet<Recipe> Recipes { get; init; }
+    public DbSet<RecipeIngredientItem> RecipeIngredientItems { get; init; }
+    public DbSet<IngredientCatalogItem> IngredientCatalogItems { get; init; }
+    public DbSet<City> Cities { get; init; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder builder)
     {
