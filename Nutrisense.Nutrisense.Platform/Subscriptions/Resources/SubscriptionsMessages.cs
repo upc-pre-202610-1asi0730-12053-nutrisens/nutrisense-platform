2c0919820b531f62054193b5553f93e47516d247
Joel Fernando Mora Rivera <joelfmr637@gmail.com>
2026-07-01 16:03:40 -0500

    feat(subscriptions): add localized subscriptions messages resources

---
+++ Nutrisense.Nutrisense.Platform/Subscriptions/Resources/SubscriptionsMessages.cs
@@ -0,0 +1,8 @@
+namespace Nutrisense.Nutrisense.Platform.Subscriptions.Resources;
+
+// Marker class for the Subscriptions bounded context localization resources.
+// Resolves SubscriptionsMessages.resx (English, neutral) and SubscriptionsMessages.es.resx (Spanish).
+// ReSharper disable once ClassNeverInstantiated.Global
+public class SubscriptionsMessages
+{
+}
