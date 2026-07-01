b6a40e241e79b684d4a3ad4b2ff45a53a3448376
roseal28 <rosealmendra.calderon@gmail.com>
2026-07-01 16:26:07 -0500

    refactor(smart-recommendations): remove per-command application errors superseded by SmartRecommendationsError

--- Nutrisense.Nutrisense.Platform/SmartRecommendations/Application/Errors/RecipeImportError.cs
+++
@@ -1,7 +0,0 @@
-namespace Nutrisense.Nutrisense.Platform.SmartRecommendations.Application.Errors;
-
-public enum RecipeImportError
-{
-    InsufficientIngredients,
-    UnexpectedError
-}
--- Nutrisense.Nutrisense.Platform/SmartRecommendations/Application/Errors/RemovePantryItemError.cs
+++
@@ -1,7 +0,0 @@
-namespace Nutrisense.Nutrisense.Platform.SmartRecommendations.Application.Errors;
-
-public enum RemovePantryItemError
-{
-    ItemNotFound,
-    UnexpectedError
-}
