b11dd5dd3303a5715caf70788683ecf2f7eb6b32
Emy127 <166949210+Emy127@users.noreply.github.com>
2026-07-01 17:07:47 -0500

    refactor(body-health-metrics): remove per-command application errors superseded by BodyHealthMetricsError

--- Nutrisense.Nutrisense.Platform/BodyHealthMetrics/Application/Errors/RegisterBodyMeasurementError.cs
+++
@@ -1,9 +0,0 @@
-namespace Nutrisense.Nutrisense.Platform.BodyHealthMetrics.Application.Errors;
-
-/// <summary>Failure cases for the register-body-measurement use case.</summary>
-public enum RegisterBodyMeasurementError
-{
-    BodyMetricsNotFound,
-    InvalidData,
-    UnexpectedError
-}
--- Nutrisense.Nutrisense.Platform/BodyHealthMetrics/Application/Errors/RegisterBodyMetricsError.cs
+++
@@ -1,9 +0,0 @@
-namespace Nutrisense.Nutrisense.Platform.BodyHealthMetrics.Application.Errors;
-
-/// <summary>Failure cases for the register-body-metrics use case.</summary>
-public enum RegisterBodyMetricsError
-{
-    AlreadyExists,
-    InvalidData,
-    UnexpectedError
-}
