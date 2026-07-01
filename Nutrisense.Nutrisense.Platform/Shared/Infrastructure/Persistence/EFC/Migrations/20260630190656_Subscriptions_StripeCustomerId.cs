f493d3e319e0b2ed14683476718a9983cb10ceac
Joel Fernando Mora Rivera <joelfmr637@gmail.com>
2026-07-01 16:03:24 -0500

    feat(subscriptions): add EF migration for stripe customer id

---
+++ Nutrisense.Nutrisense.Platform/Shared/Infrastructure/Persistence/EFC/Migrations/20260630190656_Subscriptions_StripeCustomerId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nutrisense.Nutrisense.Platform.Shared.Infrastructure.Persistence.EFC.Migrations
+{
+    /// <inheritdoc />
+    public partial class Subscriptions_StripeCustomerId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "stripe_customer_id",
+                table: "user_subscriptions",
+                type: "longtext",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "stripe_customer_id",
+                table: "user_subscriptions");
+        }
+    }
+}
